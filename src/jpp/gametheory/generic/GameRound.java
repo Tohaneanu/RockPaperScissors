4b6e09d1b822c5a950dd8e96c0fbf5cdb5a7f8b6
Tohaneanu Andrei <andrei.tohaneanu.kes@gmail.com>
2022-08-20 13:22:25 +0300

    GameRound

--- src/jpp/gametheory/generic/GameRound.java
+++ src/jpp/gametheory/generic/GameRound.java
@@ -1,36 +1,57 @@
 package jpp.gametheory.generic;
 
+import jpp.gametheory.rockPaperScissors.RPSChoice;
+
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class GameRound<C extends IChoice> implements IGameRound<C> {
+    private Map<IPlayer<C>, C> playerChoices;
 
     public GameRound(Map<IPlayer<C>, C> playerChoices) {
-        throw new UnsupportedOperationException();
+        if (playerChoices == null)
+            throw new NullPointerException("The map is null!");
+        if (playerChoices.keySet().size() < 1)
+            throw new IllegalArgumentException("The map contains less than one player!");
+        this.playerChoices = playerChoices;
     }
 
     @Override
     public Map<IPlayer<C>, C> getPlayerChoices() {
-        throw new UnsupportedOperationException();
+        return this.playerChoices;
     }
 
     @Override
     public C getChoice(IPlayer<C> player) {
-        throw new UnsupportedOperationException();
+        if (player == null || !playerChoices.containsKey(player))
+            throw new NullPointerException("Player cannot be null, or player does not exist in game!");
+        return this.playerChoices.get(player);
     }
 
     @Override
     public Set<IPlayer<C>> getPlayers() {
-        throw new UnsupportedOperationException();
+        return this.playerChoices.keySet();
     }
 
     @Override
     public Set<IPlayer<C>> getOtherPlayers(IPlayer<C> player) {
-        throw new UnsupportedOperationException();
+        if (player == null || !playerChoices.containsKey(player))
+            throw new NullPointerException("Player cannot be null, or player does not exist in game!");
+        Set<IPlayer<C>> players = this.playerChoices.keySet();
+        players.remove(player);
+        return players;
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException();
+        SortedMap<IPlayer<C>, C> sortedMap = new TreeMap<>(playerChoices);
+        StringBuilder result=new StringBuilder();
+        result.append("(");
+        for (Map.Entry<IPlayer<C>, C> entry : sortedMap.entrySet()){
+            result.append(entry.getKey().getName()).append("->").append(entry.getValue().name()).append(", ");
+        }
+        return result.toString();
     }
 }
